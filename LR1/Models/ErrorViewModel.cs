6fd44c11792d6862d8439a18e73bd92620afaed3
Shapovalov24 <litdan03@gmail.com>
2024-10-20 23:41:32 +0300

    Added layout and handle errors

---
+++ LR1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shapovalov.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
